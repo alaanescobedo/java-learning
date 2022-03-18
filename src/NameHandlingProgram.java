a7a7be15a5aa38540151bda991649fdbc5e4a41c
Alan Escobedo <alaanprogramacion@gmail.com>
2022-03-18 11:22:24 -0600

    Refactor into variables and close the scanner

--- src/NameHandlingProgram.java
+++ src/NameHandlingProgram.java
@@ -5,8 +5,9 @@ public class NameHandlingProgram {
         Scanner scanner = new Scanner(System.in);
 
         String name1, name2, name3;
-        name1 = name2 = name3 = "";
         char separator = '_';
+
+        name1 = name2 = name3 = "";
 
         try {
             System.out.print("Enter first name: ");
@@ -15,6 +16,8 @@ public class NameHandlingProgram {
             name2 = transformName(scanner.nextLine());
             System.out.print("Enter third name: ");
             name3 = transformName(scanner.nextLine());
+
+            scanner.close();
         } catch (Exception e) {
             System.out.println("Invalid input!");
             main(args);
@@ -30,10 +33,17 @@ public class NameHandlingProgram {
 }
 
 /*
-La tarea consiste en crear una clase llamada ProgramaManejoDeNombres de la siguiente manera:
-Se requiere desarrollar un programa que reciba 3 nombres como argumentos de línea de comandos.
-- Se pide por cada nombre de la persona una nueva variable del tipo String al tomar el segundo carácter, pero convertido en mayúscula y se le concatena un punto y los dos últimos caracteres de la persona.
-    Por ejemplo para Andres debe quedar como: N.es
-- Debe imprimir como resultado los tres nuevos nombres separado con guion bajo (como una única variable).
-    Ejemplo, un resultado final esperado para los nombres Andres, Maria y Pepe podría ser: N.es_A.ia_E.pe
-*/
+ * La tarea consiste en crear una clase llamada ProgramaManejoDeNombres de la
+ * siguiente manera:
+ * 
+ * - Se requiere desarrollar un programa que reciba 3 nombres como argumentos de
+ * línea de comandos.
+ * - Se pide por cada nombre de la persona una nueva variable del tipo String al
+ * tomar el segundo carácter, pero convertido en mayúscula y se le concatena un
+ * punto y los dos últimos caracteres de la persona.
+ * -- Por ejemplo para Andres debe quedar como: N.es
+ * - Debe imprimir como resultado los tres nuevos nombres separado con guion
+ * bajo (como una única variable).
+ * -- Ejemplo, un resultado final esperado para los nombres Andres, Maria y Pepe
+ * podría ser: N.es_A.ia_E.pe
+ */
